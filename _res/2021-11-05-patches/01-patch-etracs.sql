275381e0ed09ce5765e7b8a9c7e60b1dc3421629
worgflores <worgflores@gmail.com>
2021-11-05 22:21:36 +0800

    _res: patches: updated

--- _res/2021-11-05-patches/01-patch-etracs.sql
+++ _res/2021-11-05-patches/01-patch-etracs.sql
@@ -43,3 +43,31 @@ VALUES ('038CAT', 'CATICLAN JETTY PORT TERMINAL', 'TERMINAL', '038', 'PROVINCE',
 
 INSERT INTO sys_var (name, value, description, datatype, category) 
 VALUES ('treasury_remote_orgs', '038CAG,038CAT', NULL, NULL, 'TC');
+
+
+INSERT INTO itemaccount (
+  objid, state, type, 
+  code, title, description, 
+  org_objid, org_name, defaultvalue, valuetype, 
+  parentid, generic, sortorder, hidefromlookup, 
+  fund_objid, fund_code, fund_title   
+) 
+select t0.*, 
+  fund.objid as fund_objid, fund.code as fund_code, fund.title as fund_title  
+from ( 
+  select  
+    'AKLAN_TERMINAL_ONLINE_CATICLAN' as objid, 'ACTIVE' as state, 'REVENUE' as type, 
+    '623_CJPPT' as code, 'AKLAN TERMINAL (ONLINE) - CATICLAN' as title, 'AKLAN TERMINAL (ONLINE) - CATICLAN' as description, 
+    '038CAT' as org_objid, 'CATICLAN JETTY PORT TERMINAL' as org_name, 0.00 as defaultvalue, 'ANY' as valuetype, 
+    NULL as parentid, 0 as generic, 0 as sortorder, 0 as hidefromlookup 
+
+  union 
+
+  select 
+    'AKLAN_TERMINAL_ONLINE_CAGBAN' as objid, 'ACTIVE' as state, 'REVENUE' as type, 
+    '623_CJPPT' as code, 'AKLAN TERMINAL (ONLINE) - CAGBAN' as title, 'AKLAN TERMINAL (ONLINE) - CAGBAN' as description, 
+    '038CAG' as org_objid, 'CAGBAN JETTY PORT TERMINAL' as org_name, 0.00 as defaultvalue, 'ANY' as valuetype, 
+    NULL as parentid, 0 as generic, 0 as sortorder, 0 as hidefromlookup 
+)t0, fund 
+where fund.title = 'EEDD' 
+;
